d6e985de6d10cb3e4ff9c257933cadb52aeb25f0
gurayyalmaz <guray.yl93@gmail.com>
2024-09-22 01:32:31 +0200

    Initial Commit

---
+++ BasicShoppingApp/BasicShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicShoppingAppApp.swift
+//  BasicShoppingApp
+//
+//  Created by Gyuray Yalmaz on 22.09.24.
+//
+
+import SwiftUI
+
+@main
+struct BasicShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
